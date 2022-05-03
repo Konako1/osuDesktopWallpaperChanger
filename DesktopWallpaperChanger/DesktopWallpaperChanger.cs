89bce1a166460773a7847ee6f185b75d7f2489c5
Konako <konako.osu@gmail.com>
2022-05-03 16:02:57 +0500

    Initial commit

---
+++ DesktopWallpaperChanger/DesktopWallpaperChanger.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using Microsoft.Win32;
+
+namespace DesktopWallpaperChanger
+{
+    class WallpaperChanger
+    {
+        //stolen
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+        //endstolen
+
+        public string CurrentMapArtist;
+        public string CurrentMapTitle;
+        public string CurrentMapMapper;
+        
+        public void ChangeWallpaper(object songsFolder)
+        {
+            var osuDirectory = new DirectoryInfo((string)songsFolder);
+            var directories = osuDirectory.GetDirectories();
+            var isUpdated = false;
+            FileInfo[] mapFiles = null;
+            while (!isUpdated)
+            {
+                var map = PickMap(directories);
+                mapFiles = map.GetFiles();
+                var bg = GetMapBackground(mapFiles);
+                if (bg == null)
+                    continue;
+                var result = SetWallpaper(bg);
+                isUpdated = result == 1;
+            }
+            FillMapData(mapFiles);
+        }
+        
+        //stolen
+        private static int SetWallpaper(FileInfo file)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            key.SetValue(@"WallpaperStyle", 1.ToString());
+            key.SetValue(@"TileWallpaper", 0.ToString());
+            return SystemParametersInfo(
+                SPI_SETDESKWALLPAPER, 
+                0,
+                file.FullName, 
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE
+                );
+        }
+        //endstolen
+
+        private static FileInfo GetMapBackground(FileInfo[] mapFiles)
+        {
+            var pics = mapFiles.Where(file => file.Extension == ".png" || file.Extension == ".jpg" || file.Extension == ".jpeg").ToList();
+            
+            if (!pics.Any() || pics.Exists(file => file.Extension == ".osu"))
+            {
+                Console.WriteLine("List is empty");
+                return null;
+            }
+            
+            var wallpaperFile = pics[0];
+            foreach (var pic in pics)
+            {
+                if (pic.Length > wallpaperFile.Length)
+                    wallpaperFile = pic;
+            }
+            return wallpaperFile;
+        }
+
+        private static DirectoryInfo PickMap(DirectoryInfo[] directories)
+        {
+            var dirCount = directories.Length;
+            var rnd = new Random();
+            return directories[rnd.Next(0, dirCount)];
+        }
+
+        private void FillMapData(FileInfo[] mapFiles)
+        {
+            var pattern = @"(.+) - (.+) \((.+)\) \[(.+)\].*";
+            var fileName = mapFiles.First(file => file.Extension == ".osu").Name;
+            foreach (Match match in Regex.Matches(fileName, pattern))
+            {
+                CurrentMapArtist = match.Groups[1].Value;
+                CurrentMapTitle = match.Groups[2].Value;
+                CurrentMapMapper = match.Groups[3].Value;
+            }
+        }
+    }
+}
---
+++ DesktopWallpaperChanger/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DesktopWallpaperChanger.Properties;
+using IniParser;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace DesktopWallpaperChanger
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new AppContext());
+        }
+    }
+
+    public class AppContext : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+        private System.Threading.Timer timer;
+        private int sleepTime = 30*60*1000;
+        private WallpaperChanger wc;
+        public AppContext()
+        {
+            wc = new WallpaperChanger();
+            trayIcon = new NotifyIcon()
+            {
+                Icon = Resources.favicon,
+                Visible = true,
+                ContextMenuStrip = new ContextMenuStrip()
+                {
+                    Items =
+                    {
+                        new ToolStripMenuItem("Change wallpaper", null, ChangeWallpaper),
+                        new ToolStripMenuItem("Copy map data to clipboard", null, CopyMapInfoToClipboard),
+                        new ToolStripMenuItem("Exit", null, Exit),
+                    }
+                },
+                Text = "osu! Wallpaper Changer",
+            };
+            trayIcon.Click += ChangeWallpaper;
+            var songsFolder = CheckCfg();
+            timer = new System.Threading.Timer(wc.ChangeWallpaper, songsFolder, 0, sleepTime);
+        }
+
+        private string CheckCfg()
+        {
+            var parser = new FileIniDataParser();
+            var dir = Directory.GetCurrentDirectory();
+            if (!File.Exists( dir + "/cfg.ini"))
+                CreateCgfFile(parser, dir);
+            var cfgData = parser.ReadFile("cfg.ini");
+            var songsFolder = cfgData["Config"]["SongsFolder"];
+            return songsFolder;
+        }
+
+        private void CreateCgfFile(FileIniDataParser parser, string dir)
+        {
+            var file = File.Create(dir + "/cfg.ini");
+            file.Close();
+            var data = parser.ReadFile("cfg.ini");
+            data.Sections.AddSection("Config");
+            var songsFolderPath = GetSongsFolderPath();
+            if (songsFolderPath == null)
+            {
+                TrueExit();
+                return;
+            }
+            data["Config"].AddKey("SongsFolder", songsFolderPath);
+            parser.WriteFile("cfg.ini", data);
+        }
+        
+        private string? GetSongsFolderPath()
+        {
+            var fbd = new CommonOpenFileDialog();
+            fbd.IsFolderPicker = true;
+            var result = fbd.ShowDialog();
+            if (result == CommonFileDialogResult.Ok)
+                return fbd.FileName;
+            return null;
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            TrueExit();
+        }
+
+        private void TrueExit()
+        {
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+
+        private void ChangeWallpaper(object sender, EventArgs e)
+        {
+            MouseEventArgs me;
+            try
+            {
+                me = (MouseEventArgs) e;
+            }
+            catch(InvalidCastException)
+            {
+                timer.Change(0, sleepTime);
+                return;
+            }
+            if ((me.Button & MouseButtons.Left) != 0)
+                timer.Change(0, sleepTime);
+        }
+
+        private void CopyMapInfoToClipboard(object sender, EventArgs e)
+        {
+            Clipboard.SetText($"{wc.CurrentMapArtist} - {wc.CurrentMapTitle} by {wc.CurrentMapMapper}");
+        }
+    }
+}
